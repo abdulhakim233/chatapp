f3fe0c5f91685e0d55d8f8cde4bb714a68214156
Shehabedin Ahmed Ali <shahabahmed20001@gmail.com>
2024-12-09 22:31:33 +0300

    added timestamp for sent messages in the chat page.

--- lib/components/chat_bubble.dart
+++ lib/components/chat_bubble.dart
@@ -14,13 +14,34 @@ class ChatBubble extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-          color: isCurrentUser ? Colors.green : Colors.grey.shade500,
-          borderRadius: BorderRadius.circular(12)),
+        color: isCurrentUser ? Colors.green : Colors.grey.shade500,
+        borderRadius: BorderRadius.only(
+          topLeft: isCurrentUser
+              ? const Radius.circular(12)
+              : const Radius.circular(0),
+          topRight: isCurrentUser
+              ? const Radius.circular(0)
+              : const Radius.circular(12),
+          bottomLeft: const Radius.circular(12),
+          bottomRight: const Radius.circular(12),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            offset: const Offset(2, 2),
+            blurRadius: 5,
+          ),
+        ],
+      ),
       padding: const EdgeInsets.all(12),
       margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 2),
-      child: Text(
-        message,
-        style: const TextStyle(fontSize: 16, color: Colors.white),
+      child: ConstrainedBox(
+        constraints:
+            BoxConstraints(maxWidth: MediaQuery.of(context).size.width * 0.7),
+        child: Text(
+          message,
+          style: const TextStyle(fontSize: 16, color: Colors.white),
+        ),
       ),
     );
   }
